8fc14d9931af23ac33c094b4874298cd32f77e9e
djzevenbergen <djzevenbergen@gmail.com>
2020-04-03 09:51:21 -0700

    add beep! spec for all numbers containing 1

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,38 @@ var createArray = function (number, rev) {
     return numArray;
   }
 
-
-
 }
 
+var changeArray = function (array) {
+  var localArrays = array.slice();
+  var newArray = [];
 
+  localArrays.forEach(function (locArr) {
+    var contains = false;
+    //alert(locArr);
+    var lo = locArr.toString();
+    var sp = lo.split('');
+    //alert(locArr.toString());
+    for (i = 0; i < sp.length; i++) {
+      //alert(locArr);
+      if (sp[i] === '1') {
+        contains = true;
+      }
+      if (contains === true) {
+        newArray.push("Beep!")
+        //alert("hi");
+
+      } else {
+        newArray.push(locArr);
+      }
+    };
+
+
+
+  });
+  return newArray;
+
+};
 
 
 
@@ -27,7 +54,12 @@ $(document).ready(function () {
     event.preventDefault();
     var userNumber = parseInt($("#numberForm").val());
     var isReversed = document.getElementById("revCheckbox").checked;
-    alert(createArray(userNumber, isReversed));
+    //alert(createArray(userNumber, isReversed));
+
+    var userArray = createArray(userNumber, isReversed);
+    alert(userArray);
+
+    alert(changeArray(userArray));
 
 
   });
